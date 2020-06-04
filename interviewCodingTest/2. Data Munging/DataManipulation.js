95ce87a9237703b4dcc56c2b0e2682ee855de64a
John keczmerski <keczmerski@live.com>
2020-06-04 16:46:44 -0400

    Refactored with array destructuring, and replaced var with const and let. Var is legacy, const and let replace it. Added *temp* to ignore file

--- interviewCodingTest/2. Data Munging/DataManipulation.js
+++ interviewCodingTest/2. Data Munging/DataManipulation.js
@@ -1,44 +1,31 @@
-// Input Values
-var categories = "CFE,Coffee,Y\nFD,Food,Y\nPRS,Personal,N";
-var expenses = "Starbucks,3/10/2018,Iced Americano,4.28,CFE\nStarbucks,3/10/2018,Nitro Cold Brew,3.17,CFE\nStarbucks,3/10/2018,Souvineer Mug,8.19,PRS\nStarbucks,3/11/2018,Nitro Cold Brew,3.17,CFE\nHigh Point Market,3/11/2018,Iced Americano,2.75,CFE\nHigh Point Market,3/11/2018,Pastry,2.00,FD\nHigh Point Market,3/11/2018,Gift Card,10.00,PRS";
-
-var AcceptableExpenses = GetAcceptableExpenses(GetAcceptableCategories(categories), expenses)
-
-AcceptableExpenses.forEach (AcceptableExpense =>{
-    console.log(AcceptableExpense.join(''))
-})
-
 function GetAcceptableExpenses(AcceptableCategories, expenses){
-    var ExpensesList = expenses.split("\n")
-    var AcceptableExpenseTotals = []
-    var categoryIndex = 4
-    var venderIndex = 0
-    var dateIndex = 1
-    var priceIndex = 3
-
-    var venderDate = []
+    const ExpensesList = expenses.split("\n")
+    let AcceptableExpenseTotals = []
+    let venderDate = []
+    
     ExpensesList.forEach(expense => {
-        var expenseItemized = expense.split(',')
+        const [vender,date,,price,category] = expense.split(',')
 
-        if ((categoryIndex < expenseItemized.length) && IsExpenseItemAcceptable(AcceptableCategories, expenseItemized[categoryIndex])){
-            venderDate = [expenseItemized[dateIndex], ": ", expenseItemized[venderIndex], " - $"].join('')
+        if ( AcceptableCategories.includes(category)){
+            venderDate = [date, ": ", vender, " - $"].join('')
 
             if ( IsVenderDateIncluded(AcceptableExpenseTotals, venderDate) ){
-                UpdatePrice(AcceptableExpenseTotals, venderDate, expenseItemized[priceIndex] )
+                UpdatePrice(AcceptableExpenseTotals, venderDate, price )
             }
             else{
-                AcceptableExpenseTotals.push([venderDate, expenseItemized[priceIndex] ])
+                AcceptableExpenseTotals.push({dateVender: venderDate, totalPrice: price })
             }
         }
     })
     return AcceptableExpenseTotals
 }
+
 function IsVenderDateIncluded(AcceptableExpenseTotals, venderDate){
-    var venderDateIndex = 0
-    var IsIncluded = false
+    const venderDateIndex = 0
+    let IsIncluded = false
   
     AcceptableExpenseTotals.forEach( AcceptableExpenseTotal =>{
-        if (AcceptableExpenseTotal[venderDateIndex] === venderDate){
+        if (AcceptableExpenseTotal['dateVender'] === venderDate){
             IsIncluded = true
         }
     })
@@ -47,39 +34,50 @@ function IsVenderDateIncluded(AcceptableExpenseTotals, venderDate){
 }
 
 function UpdatePrice(AcceptableExpenseTotals, venderDate, price ){
-    var venderDateIndex = 0
-    var priceIndex = 1
     AcceptableExpenseTotals.forEach( AcceptableExpenseTotal =>{
-        if (AcceptableExpenseTotal[venderDateIndex] === venderDate){
-            AcceptableExpenseTotal[priceIndex] = parseFloat( price) + parseFloat(AcceptableExpenseTotal[priceIndex])
+        if (AcceptableExpenseTotal['dateVender'] === venderDate){
+            AcceptableExpenseTotal['totalPrice'] = parseFloat( price) + parseFloat(AcceptableExpenseTotal['totalPrice'])
             return
         }
     })
 }
 
-function IsExpenseItemAcceptable(AcceptableCategories, category ){
-    var IsAcceptable = false
-    AcceptableCategories.forEach(AcceptableCategory => {
-        if (AcceptableCategory === category){
-            IsAcceptable = true
-        }
-    })
-    return IsAcceptable
-}
-
 function GetAcceptableCategories(categories){
-    var categoryList = categories.split("\n")
-    var AcceptableCategory = []
+    const categoryList = categories.split("\n")
+    const AcceptableCategory = []
 
     for (i = 0; i < categoryList.length; i++){
-        var categoryData = categoryList[i].split(',');
-        var locationOf_IsExpensible = 2;
-        var locationOf_Category = 0;
-        if (locationOf_IsExpensible < categoryData.length ){
-            if (categoryData[locationOf_IsExpensible]==="Y"){
-                AcceptableCategory.push(categoryData[locationOf_Category])
-            }
+        const [Category,,IsExpensible] = categoryList[i].split(',');
+
+        if (IsExpensible === "Y" ){
+                AcceptableCategory.push(Category)
         }
     }
     return AcceptableCategory
 }
+
+// Input Values
+const categories = "CFE,Coffee,Y\nFD,Food,Y\nPRS,Personal,N";
+const expenses = "Starbucks,3/10/2018,Iced Americano,4.28,CFE\nStarbucks,3/10/2018,Nitro Cold Brew,3.17,CFE\nStarbucks,3/10/2018,Souvineer Mug,8.19,PRS\nStarbucks,3/11/2018,Nitro Cold Brew,3.17,CFE\nHigh Point Market,3/11/2018,Iced Americano,2.75,CFE\nHigh Point Market,3/11/2018,Pastry,2.00,FD\nHigh Point Market,3/11/2018,Gift Card,10.00,PRS";
+// Acceptable: 
+//------------------------------------------------
+//        Starbucks,        3/10/2018,Iced Americano, 4.28,CFE
+//        Starbucks,        3/10/2018,Nitro Cold Brew,3.17,CFE
+// TOTAL: Starbucks,        3/10/2018,***,            7.45,CFE *** Total
+//        Starbucks,        3/11/2018,Nitro Cold Brew,3.17,CFE
+// TOTAL: Starbucks,        3/11/2018,Nitro Cold Brew,3.17,CFE *** Total
+//        High Point Market,3/11/2018,Iced Americano, 2.75,CFE
+//        High Point Market,3/11/2018,Pastry,         2.00,FD
+// TOTAL: High Point Market,3/11/2018,***,            4.75,FD  *** Total
+// Not Acceptable
+//------------------------------------------------
+// Starbucks,        3/10/2018,Souvineer Mug, 8.19,PRS
+// High Point Market,3/11/2018,Gift Card,    10.00,PRS
+
+const AcceptableExpenses = GetAcceptableExpenses(GetAcceptableCategories(categories), expenses)
+
+AcceptableExpenses.forEach (AcceptableExpense =>{
+    console.log([AcceptableExpense['dateVender'], AcceptableExpense['totalPrice'] ].join(''))
+})
+console.log("Processing complete")
+
