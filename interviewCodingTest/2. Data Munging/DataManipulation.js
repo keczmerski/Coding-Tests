c45e6c87edd760fa918095e0a76969fb00adf5a3
John keczmerski <keczmerski@live.com>
2020-06-15 23:08:27 -0400

    Changed to use OOP. Credit https://codereview.stackexchange.com/a/243346/57486 for inspiring changes.

--- interviewCodingTest/2. Data Munging/DataManipulation.js
+++ interviewCodingTest/2. Data Munging/DataManipulation.js
@@ -1,40 +1,53 @@
-function GetAcceptableExpenses(AcceptableCategories, expenses){
-    const ExpensesList = expenses.split("\n")
-    let AcceptableExpenseTotals = {};
-    let venderDate = []
-    
-    ExpensesList.forEach(expense => {
-        const [vender,date,,price,category] = expense.split(',')
+class AcceptableExpenceSheet{
+    constructor(categories, expenses) {
+        this.AcceptableCategories = this.GetAcceptableCategories(categories);
+        this.AcceptableExpenses = this.GetAcceptableExpenses (expenses)
+      }
 
-        if ( AcceptableCategories.includes(category)){
-            venderDate = [date, ": ", vender, " - $"].join('')
+    GetAcceptableExpenses(expenses){
+        const ExpensesList = expenses.split("\n")
+        let AcceptableExpenseTotals = {};
+        let venderDate = []
+        
+        ExpensesList.forEach(expense => {
+            const [vender,date,,price,category] = expense.split(',')
 
-            if ( venderDate in AcceptableExpenseTotals){
-                AcceptableExpenseTotals[venderDate] += parseFloat(price);
+            if ( this.AcceptableCategories.includes(category)){
+                venderDate = [date, ": ", vender, " - $"].join('')
+
+                if ( venderDate in AcceptableExpenseTotals){
+                    AcceptableExpenseTotals[venderDate] += parseFloat(price);
+                }
+                else{
+                    AcceptableExpenseTotals[venderDate] = parseFloat(price);
+                }
             }
-            else{
-                AcceptableExpenseTotals[venderDate] = parseFloat(price);
-            }
-        }
-    })
-    return AcceptableExpenseTotals
-}
-
-
-function GetAcceptableCategories(categories){
-    const categoryList = categories.split("\n")
-    const AcceptableCategory = []
-
-    for (i = 0; i < categoryList.length; i++){
-        const [Category,,IsExpensible] = categoryList[i].split(',');
-
-        if (IsExpensible === "Y" ){
-                AcceptableCategory.push(Category)
-        }
+        })
+        return AcceptableExpenseTotals
     }
-    return AcceptableCategory
-}
 
+    GetAcceptableCategories(categories){
+        const categoryList = categories.split("\n")
+        const AcceptableCategory = []
+
+        for (let i = 0; i < categoryList.length; i++){
+            const [Category,,IsExpensible] = categoryList[i].split(',');
+
+            if (IsExpensible === "Y" ){
+                    AcceptableCategory.push(Category)
+            }
+        }
+        return AcceptableCategory
+    }
+
+    Print(){
+        const keys = Object.keys(this.AcceptableExpenses);
+        
+        keys.forEach((key, index) => {
+            console.log(`${key} ${this.AcceptableExpenses[key]}`);
+        });
+    }
+}
 // Input Values
 const categories = "CFE,Coffee,Y\nFD,Food,Y\nPRS,Personal,N";
 const expenses = "Starbucks,3/10/2018,Iced Americano,4.28,CFE\nStarbucks,3/10/2018,Nitro Cold Brew,3.17,CFE\nStarbucks,3/10/2018,Souvineer Mug,8.19,PRS\nStarbucks,3/11/2018,Nitro Cold Brew,3.17,CFE\nHigh Point Market,3/11/2018,Iced Americano,2.75,CFE\nHigh Point Market,3/11/2018,Pastry,2.00,FD\nHigh Point Market,3/11/2018,Gift Card,10.00,PRS";
@@ -53,13 +66,8 @@ const expenses = "Starbucks,3/10/2018,Iced Americano,4.28,CFE\nStarbucks,3/10/20
 // Starbucks,        3/10/2018,Souvineer Mug, 8.19,PRS
 // High Point Market,3/11/2018,Gift Card,    10.00,PRS
 
-const AcceptableExpenses = GetAcceptableExpenses(GetAcceptableCategories(categories), expenses)
-
-const keys = Object.keys(AcceptableExpenses);
-
-keys.forEach((key, index) => {
-    console.log(`${key} ${AcceptableExpenses[key]}`);
-});
+ExpenseCalculation = new AcceptableExpenceSheet(categories, expenses);
+ExpenseCalculation.Print();
 
 console.log("Processing complete")
 
